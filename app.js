c2b3b492075f962097581882d71f84abcae5a7c5
Hajigil <37492940+Hajigil@users.noreply.github.com>
2021-01-03 18:28:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
 });
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+});
+
+//Javascript Animated Navbar Tutorial by Dev Ed channel on youtube
